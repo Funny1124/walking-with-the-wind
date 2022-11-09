64137dcd2f8ef71a8d4f75d259be3eb9552e88d7
Cyan <204267667@qq.com>
2022-11-09 11:42:23 +0800

    登录界面修改

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="form">
-    <h2>
-      <span>登录</span>
-    </h2>
-    <div class="item">
-      <input type="text" placeholder="账号">
-    </div>
-    <div class="item">
-      <input type="password" placeholder="密码">
-    </div>
-    <div class="button">
-      <button>登录</button>
-    </div>
+    <form>
+      <img class="mb-4" src="../assets/logo.png" alt="" width="108" height="108">
+      <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+      <div class="form-floating item-margin">
+        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <label for="floatingInput">Email address</label>
+      </div>
+      <div class="form-floating item-margin">
+        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+        <label for="floatingPassword">Password</label>
+      </div>
+
+      <div class="checkbox mb-3 item-margin">
+        <label>
+          <input type="checkbox" value="remember-me"> Remember me
+        </label>
+      </div>
+      <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+      <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
+    </form>
   </div>
 </template>
 
@@ -22,51 +31,17 @@ export default {
 </script>
 
 <style scoped>
-.form{
+.form {
   display: block;
   margin: 10% auto;
   width: 20%;
   min-width: 420px;
   /*background-color: rgb(94,93,91,.11);*/
   border-radius: 6mm;
-  background-color: rgba(246, 241, 237, 0.98);
-}
-.form h2{
   text-align: center;
-  font-size: 30px;
-  color: black;
-  border-bottom: 1px solid rgb(105,105,105);
-  height: 50px;
-  margin-bottom: 40px;
 }
-input[type="text"],input[type="password"]{
-  border: 1px solid #dcdfe6;
-  width: 75%;
-  box-sizing: border-box;
-  border-radius: 3px;
-  height: 50px;
-  text-indent: 1em;
-  font-size: 14px;
-  color: black;
-  margin: 16px 48px;
-  background-color: rgb(126,117,117,.20);
-  /*background-image: linear-gradient(95deg,rgba(111,399,99,0.61),rgba(111,4557,180,0.5));*/
-  background-image: linear-gradient(95deg,rgba(114,114,202,.3),rgba(90,187,182,0.5));
-}
-input[type="text"]::placeholder,input[type="password"]::placeholder{
-  color: black;
-}
-input[type="text"]:hover,input[type="password"]:hover{
-  border-color: #33476e;
-}
-.form .button{
 
-}
-button{
-  width: 85px;
-  height: 30px;
-  margin: 45px 170px;
-  background-color: #dcdfe6;
-  background-image: linear-gradient(95deg,rgba(114,114,202,.3),rgba(90,187,182,0.5));
+.item-margin {
+  margin-top: 10px;
 }
 </style>
