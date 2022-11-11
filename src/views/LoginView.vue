a81d0df16f74de38d63e1ecd1e3cc2291ff516f3
Cyan <204267667@qq.com>
2022-11-11 21:15:02 +0800

    登录页修改

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,26 +1,31 @@
 <template>
-  <div class="form">
-    <form>
-      <img class="mb-4" src="../assets/logo.png" alt="" width="108" height="108">
-      <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
-      <div class="form-floating item-margin">
-        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-        <label for="floatingInput">Email address</label>
+  <div class="container col-xl-10 col-xxl-8 px-4 py-5">
+    <div class="row align-items-center g-lg-5 py-5">
+      <div class="col-lg-7 text-center text-lg-start">
+        <img src="../assets/logo.png" alt="logo" width="60%">
       </div>
-      <div class="form-floating item-margin">
-        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-        <label for="floatingPassword">Password</label>
+      <div class="col-md-10 mx-auto col-lg-5">
+        <form class="p-4 p-md-5 border rounded-3 bg-light">
+          <div class="form-floating mb-3">
+            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <label for="floatingInput">Email address</label>
+          </div>
+          <div class="form-floating mb-3">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <label for="floatingPassword">Password</label>
+          </div>
+          <div class="checkbox mb-3">
+            <label>
+              <input type="checkbox" value="remember-me"> Remember me
+            </label>
+          </div>
+          <button class="w-100 btn btn-lg btn-primary" type="submit">Sign up</button>
+          <hr class="my-4">
+          <small class="text-muted">By clicking Sign up, you agree to the terms of use.</small>
+        </form>
       </div>
-
-      <div class="checkbox mb-3 item-margin">
-        <label>
-          <input type="checkbox" value="remember-me"> Remember me
-        </label>
-      </div>
-      <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-      <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
-    </form>
+    </div>
   </div>
 </template>
 
@@ -31,17 +36,4 @@ export default {
 </script>
 
 <style scoped>
-.form {
-  display: block;
-  margin: 10% auto;
-  width: 20%;
-  min-width: 420px;
-  /*background-color: rgb(94,93,91,.11);*/
-  border-radius: 6mm;
-  text-align: center;
-}
-
-.item-margin {
-  margin-top: 10px;
-}
 </style>
