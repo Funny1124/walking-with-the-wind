4c65322394b20c42aec96beb95db323654f99062
Cyan <204267667@qq.com>
2022-11-14 22:39:53 +0800

    商城编辑修正（all over）

--- src/views/ShopManagement.vue
+++ src/views/ShopManagement.vue
@@ -57,7 +57,42 @@
           <el-input v-model="temp_item.type" autocomplete="off"></el-input>
         </el-form-item>
         <el-form-item label="图片" :label-width="formLabelWidth">
-          <el-input v-model="temp_item.image" autocomplete="off"></el-input>
+          <el-upload
+            action="#"
+            list-type="picture-card"
+            :auto-upload="false"
+            :file-list="file_list"
+          >
+            <i slot="default" class="el-icon-plus"></i>
+            <div slot="file" slot-scope="{file}">
+              <img
+                class="el-upload-list__item-thumbnail"
+                :src="file.url" alt=""
+              >
+              <span class="el-upload-list__item-actions">
+                <span
+                  class="el-upload-list__item-preview"
+                  @click="handlePictureCardPreview(file)">
+                  <i class="el-icon-zoom-in"></i>
+                </span>
+                <span
+                  v-if="!disabled"
+                  class="el-upload-list__item-delete"
+                  @click="handleDownload(file)">
+                  <i class="el-icon-download"></i>
+                </span>
+                <span
+                  v-if="!disabled"
+                  class="el-upload-list__item-delete"
+                  @click="handleRemove(file)">
+                  <i class="el-icon-delete"></i>
+                </span>
+              </span>
+            </div>
+          </el-upload>
+          <el-dialog :visible.sync="dialogVisible">
+            <img width="100%" :src="dialogImageUrl" alt="">
+          </el-dialog>
         </el-form-item>
         <el-form-item label="地区" :label-width="formLabelWidth">
           <el-input v-model="temp_item.region" autocomplete="off"></el-input>
